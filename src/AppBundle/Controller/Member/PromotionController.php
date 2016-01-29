7758bd06bbe95b5a79965bce26ccbd85f1dfb2d0
thomas <thomas@localhost>
2016-01-29 19:11:23 +0100

    Refactoring Member/Controllers

--- src/AppBundle/Controller/Member/PromotionController.php
+++ src/AppBundle/Controller/Member/PromotionController.php
@@ -1,5 +1,5 @@
 <?php
-namespace AppBundle\Controller;
+namespace AppBundle\Controller\Member;
 
 use AppBundle\Entity\Member;
 use AppBundle\Entity\Promotion;
@@ -14,181 +14,8 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class MemberController extends Controller
+class PromotionController extends Controller
 {
-    /**
-     * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
-     *
-     * @Route("/member/add",
-     *        name="app_member_add")
-     */
-    public function addAction(Request $request)
-    {
-        // Member manager
-        $mm = $this->get('app.member_manager');
-
-        $member = new Member();
-        $formEdit = $this->createForm(MemberType::class, $member);
-        $formEdit->handleRequest($request);
-
-        if ($formEdit->isSubmitted() && $formEdit->isValid()) {
-
-            // Save data
-            $mm->update($member);
-
-            // Save photo
-            $photo = $formEdit->get('photo')->getData();
-            if ($photo instanceof UploadedFile) {
-                $mm->updatePhoto($member, $photo);
-            }
-
-            // Flash message
-            $this->addFlash('success', $this->get('translator')->trans('add.success.added', array(), 'member'));
-
-            // Redirect
-            return $this->redirectToRoute('app_member_edit', array('member' => $member->getId()));
-
-        }
-
-        // Render
-        return $this->render('member/add.html.twig', array('formEdit' => $formEdit->createView()));
-    }
-
-    /**
-     * @param Request $request
-     * @param Member $member
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
-     *
-     * @Route("/member/edit/{member}",
-     *        name="app_member_edit",
-     *        requirements={"member"="\d+"})
-     */
-    public function editAction(Request $request, Member $member)
-    {
-        // Member manager
-        $mm = $this->get('app.member_manager');
-
-        $formEdit = $this->createForm(MemberType::class, $member);
-        $formEdit->handleRequest($request);
-
-        if ($formEdit->isSubmitted() && $formEdit->isValid()) {
-
-            // Save data
-            $mm->update($member);
-
-            // Save photo
-            $photo = $formEdit->get('photo')->getData();
-            if ($photo instanceof UploadedFile) {
-                $mm->updatePhoto($member, $photo);
-            }
-
-            // Flash message
-            $this->addFlash('success', $this->get('translator')->trans('edit.success.updated', array(), 'member'));
-
-            // Redirect
-            if ($request->request->has('update_close')) {
-                return $this->redirectToRoute('app_member_show', array('member' => $member->getId()));
-            } else {
-                return $this->redirectToRoute('app_member_edit', array('member' => $member->getId()));
-            }
-
-        }
-
-        if ($request->query->get('delete') == 'photo') {
-
-            // Remove photo
-            $mm->deletePhoto($member);
-
-            // Flash message
-            $this->addFlash(
-                'success',
-                $this->get('translator')->trans('edit.success.photo_deleted', array(), 'member')
-            );
-
-            // Redirect
-            return $this->redirectToRoute('app_member_edit', array('member' => $member->getId()));
-
-        }
-
-        // Render
-        return $this->render(
-            'member/edit.html.twig',
-            array(
-                'formEdit' => $formEdit->createView(),
-                'member' => $member,
-            )
-        );
-    }
-
-    /**
-     * @param Request $request
-     * @return Response
-     *
-     * @Route("/member",
-     *        name="app_member_index")
-     */
-    public function indexAction(Request $request)
-    {
-        $route = 'app_member_index';
-
-        // Entity manager
-        $em = $this->getDoctrine()->getManager();
-
-        // Search form
-        $formSearch = $this->createForm(MemberSearchType::class);
-
-        // Search manager
-        $sm = $this->get('app.search_manager');
-        $search = $sm->get($route, $this->getUser());
-        $reload = $search->handleRequest($request, $formSearch);
-        if ($reload) {
-            $sm->update($search);
-
-            return $this->redirectToRoute('app_member_index');
-        }
-
-        // Members
-        $members = $em->getRepository('AppBundle:Member')->findBySearch($search);
-
-        // Render
-        return $this->render(
-            'member/index.html.twig',
-            array(
-                'formSearch' => $formSearch->createView(),
-                'members' => $members,
-                'route' => $route,
-            )
-        );
-    }
-
-    /**
-     * @param Member $member
-     * @return BinaryFileResponse
-     *
-     * @Route("/member/photo/{member}",
-     *        name="app_member_photo",
-     *        requirements={"member"="\d+"})
-     */
-    public function photoAction(Member $member)
-    {
-        // Member manager
-        $mm = $this->get('app.member_manager');
-
-        // Photo
-        $photo = $mm->getPhoto($member);
-
-        if (!is_null($photo)) {
-            return new BinaryFileResponse($photo);
-        }
-
-        if ($member->getGender() == 'f') {
-            return new BinaryFileResponse($this->container->getParameter('member_photo_directory').'/female.png');
-        } else {
-            return new BinaryFileResponse($this->container->getParameter('member_photo_directory').'/male.png');
-        }
-    }
-
     /**
      * @param Request $request
      * @param Member $member
@@ -199,7 +26,7 @@ class MemberController extends Controller
      *        methods={"GET","POST"},
      *        requirements={"member"="\d+"})
      */
-    public function promotionAddAction(Request $request, Member $member)
+    public function AddAction(Request $request, Member $member)
     {
         // Member manager
         $mm = $this->get('app.member_manager');
@@ -246,7 +73,7 @@ class MemberController extends Controller
      *        methods={"GET","POST"},
      *        requirements={"promotion"="\d+"})
      */
-    public function promotionDeleteAction(Request $request, Promotion $promotion)
+    public function deleteAction(Request $request, Promotion $promotion)
     {
         // Member manager
         $mm = $this->get('app.member_manager');
@@ -291,7 +118,7 @@ class MemberController extends Controller
      *        methods={"GET","POST"},
      *        requirements={"promotion"="\d+"})
      */
-    public function promotionEditAction(Request $request, Promotion $promotion)
+    public function editAction(Request $request, Promotion $promotion)
     {
         // Member manager
         $mm = $this->get('app.member_manager');
