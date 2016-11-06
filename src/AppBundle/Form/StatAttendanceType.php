2edbfaad96390022443f2e8ba0dce01a7f09a649
thomas <thomas@localhost>
2016-11-06 08:04:22 +0100

    Clean up

--- src/AppBundle/Form/StatAttendanceType.php
+++ src/AppBundle/Form/StatAttendanceType.php
@@ -2,7 +2,6 @@
 namespace AppBundle\Form;
 
 use AppBundle\Form\Type\ChoiceSeasonType;
-use AppBundle\Form\Type\DatePickerType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
