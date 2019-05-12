979d713f041725cdb1b3366df9995d9bb0d7393c
Thomas Agé <thomas@thomasage.fr>
2019-05-12 05:54:14 +0200

    Add deployer.org Fix console

---
+++ deploy.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+namespace Deployer;
+
+require 'recipe/symfony.php';
+
+// Project repository
+set('repository', 'git@github.com:thomasage/asso.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+
+// Shared files/dirs between deploys 
+add('shared_files', []);
+add('shared_dirs', ['document']);
+
+// Writable dirs by web server 
+add('writable_dirs', []);
+set('allow_anonymous_stats', false);
+
+// Hosts
+
+inventory(__DIR__.'/hosts.yaml');
+
+// Tasks
+
+task(
+    'build',
+    function () {
+        run('cd {{release_path}} && build');
+    }
+);
+
+// [Optional] Specific to OVH
+
+set('bin/php', '/usr/local/php7.3/bin/php');
+
+// If deploy fails automatically unlock.
+
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'database:migrate');
+
